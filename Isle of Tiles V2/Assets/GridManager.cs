b3b9c9b564c5b2e1ae0a4b5db956197e922b2f8d
Jordan Grayson <jtg400@nyu.edu>
2019-06-26 10:53:38 -0700

    Second Scene to generate pre-made grids

---
+++ Isle of Tiles V2/Assets/GridManager.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GridManager : MonoBehaviour
+{
+    public const int WIDTH = 3;
+    public const int HEIGHT = 3;
+
+    public GameObject tilePrefab;
+    public GameObject gridHolder;
+
+    float xOffset = WIDTH / 2f + 4f;
+    float yOffset = HEIGHT / 2f - .75f;
+
+    public GameObject[,] tiles;
+    // Start is called before the first frame update
+    void Start()
+    {
+        tiles = new GameObject[WIDTH, HEIGHT];
+        CreateGrid();
+
+        CheckGrid();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    void CreateGrid()
+    {
+        //create the grid using a double for loop
+
+        for (int x = 0; x < WIDTH; x++)
+        {
+            for (int y = 0; y < HEIGHT; y++)
+            {
+                GameObject newTile = Instantiate(tilePrefab);
+
+                newTile.transform.parent = gridHolder.transform;
+                newTile.transform.localPosition = new Vector2(WIDTH - x - xOffset, HEIGHT - y - yOffset);
+
+                tiles[x, y] = newTile;
+
+
+            }
+        }
+
+    }
+
+    void CheckGrid()
+    {
+        for (int x = 0; x < WIDTH; x++)
+        {
+            for (int y = 0; y < HEIGHT; y++)
+            {
+                if (tiles[x + 1, y].GetComponent<TileScript>().color == tiles[x, y].GetComponent<TileScript>().color)
+                {
+                    SceneManager.LoadScene("SampleScene");
+                }
+            }
+        }
+    }
+}
---
+++ Isle of Tiles V2/Assets/TileScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileScript : MonoBehaviour
+{
+    public string color;
+    // Start is called before the first frame update
+    void Start()
+    {
+        int rand = Random.Range(1, 5);
+
+        if (rand == 1)
+        {
+            color = "blue";
+            gameObject.GetComponent<SpriteRenderer>().color = Color.cyan;
+        }
+        else if (rand == 2)
+        {
+            color = "red";
+            gameObject.GetComponent<SpriteRenderer>().color = Color.red;
+        } else if (rand == 3)
+        {
+            color = "yellow";
+            gameObject.GetComponent<SpriteRenderer>().color = Color.yellow;
+        } else if (rand == 4)
+        {
+            color = "green";
+            gameObject.GetComponent<SpriteRenderer>().color = Color.green;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
